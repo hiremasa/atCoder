4c04cde59fbe20657450f1aa9c029137e7656398
hiremasa <syorikunn0901@.com>
2020-08-19 11:45:32 +0900

    comment

--- ABC/ABC175/B.py
+++ ABC/ABC175/B.py
@@ -1,1 +1,12 @@
-print("B.py")
+N=int(input())
+L_list=list(map(int, input().split()))
+
+ans=0
+
+for i in range(N):
+	for j in range(i, N):
+		for k in range(j, N):
+			if L_list[i]!= L_list[j] and L_list[j] != L_list[k] and L_list[k] != L_list[i]:
+				if L_list[i]+L_list[j]> L_list[k] and L_list[j]+L_list[k]>L_list[i] and L_list[k]+L_list[i]>L_list[j]:
+					ans+=1
+print(ans)
