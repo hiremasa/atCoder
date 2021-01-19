53292389416ed7f16f093dbc6964829dce331893
hiremasa <syorikunn0901@gmail.com>
2021-01-19 23:43:45 +0900

    1

--- other_contents/Boot_camp_for_Beginners/Hard100/1.py
+++ other_contents/Boot_camp_for_Beginners/Hard100/1.py
@@ -1,28 +1,24 @@
-li=input()
-n=len(li)
-child_li=[0]*n
-
-
-
-ruiseki=0
-for i in range(n-1):
-    if li[i]=='R':
-        ruiseki+=1
-        if li[i+1]=='L':
-            child_li[i+1]=ruiseki//2
-            child_li[i]=(ruiseki+1)//2
+S = input()
+N = len(S)
+L = [1 if c=="L" else 0 for c in S]
+R = [1 if c=="R" else 0 for c in S]
+Ans = [0] * N
+for i, c in enumerate(S):
+    if c=="R":
+        if S[i+1]=="L":
+            Ans[i] += R[i]
+        else:
+            R[i+2] += R[i]
     else:
-        ruiseki=0
+        Ans[i] += R[i]
 
-
-ruiseki=0
-for i in range(n-1, 0, -1):
-    if li[i]=='L':
-        ruiseki+=1
-        if li[i-1]=='R':
-            child_li[i-1]+=ruiseki//2
-            child_li[i]+=(ruiseki+1)//2
+for i, c in zip(range(N-1, -1, -1), S[::-1]):
+    if c=="L":
+        if S[i-1]=="R":
+            Ans[i] += L[i]
+        else:
+            L[i-2] += L[i]
     else:
-        ruiseki=0
+        Ans[i] += L[i]
 
-print(*child_li)
+print(" ".join(map(str, Ans)))
