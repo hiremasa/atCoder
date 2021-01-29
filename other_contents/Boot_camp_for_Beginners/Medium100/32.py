6264f2b19fc7ff4f67434b56c81bf163ecb94e58
hiremasa <syorikunn0901@gmail.com>
2021-01-30 00:29:51 +0900

    32

--- other_contents/Boot_camp_for_Beginners/Medium100/32.py
+++ other_contents/Boot_camp_for_Beginners/Medium100/32.py
@@ -1,3 +1,12 @@
-N, Y = map(int, input())
+N, Y = map(int, input().split())
+Y //= 1000
 
-10a + 5b + 1c = N//1000
+"""
+10a + 5b + 1c = Y//1000
+a + b + c = N
+"""
+for a in range(Y//9 + 1):
+    for b in range((Y-a*9)//4 + 1):
+        if a + b <= N and 9*a + 4*b == Y - N:
+                exit(print(a, b, N-a-b))
+print(-1, -1, -1)
