8f4d5ebb069ef16cd9807131ba3c708df6030f6e
hiremasa <syorikunn0901@gmail.com>
2021-02-04 00:16:53 +0900

    37

--- other_contents/Boot_camp_for_Beginners/Medium100/37.py
+++ other_contents/Boot_camp_for_Beginners/Medium100/37.py
@@ -1,15 +1,16 @@
-import numpy as np
-N = int, input().split()
-H = np.array(input().split(), dtype=np.int32)
-
-
-def F(L,R):
-    if R < L:
+def merge(hight):
+    if len(hight) == 0:
         return 0
-    idx = L + H[L:R+1].argmin()
-    x = H[idx]
-    H[L:R+1] -= x
-    return x + F(L,idx-1) + F(idx+1,R)
+    if len(hight) == 1:
+        return hight[0]
+    m = min(hight)
+    m_idx = hight.index(m)
+    hight = [h - m for h in hight]
+    left_hight = hight[:m_idx]
+    right_hight = hight[m_idx + 1:]
+    return  merge(left_hight) + merge(right_hight) + m
 
-answer = F(0,N-1)
-print(answer)
+if __name__ == '__main__':
+    N = int(input())
+    h = list(map(int, input().split()))
+    print(merge(h))
