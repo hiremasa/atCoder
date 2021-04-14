634d302f4ccaa5f0fcb7068f8c7dd22f999e168f
hiremasa <syorikunn0901@gmail.com>
2021-04-14 15:50:18 +0900

    6

---
+++ other_contents/Boot_camp_for_Beginners/Hard100/6.py
@@ -0,0 +1,8 @@
+import heapq
+N, M = map(int, input().split())
+A = list(map(lambda x: int(x)*(-1), input().split()))
+heapq.heapify(A)
+for _ in range(M):
+    temp_min = heapq.heappop(A)
+    heapq.heappush(A, - (-temp_min // 2))
+print(- sum(A))
