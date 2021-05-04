7b377d07ab0089402deddfe483d523c4b7a5703b
hiremasa <syorikunn0901@gmail.com>
2021-05-04 23:01:16 +0900

    42

--- other_contents/Seisen_100/1_100/42.py
+++ other_contents/Seisen_100/1_100/42.py
@@ -1,25 +1,23 @@
-#!/usr/bin/env python3
-
-from sys import stdin
-input = stdin.readline
 
 N, M = map(int, input().split())
 D = [int(input()) for _ in range(N)]
 C = [int(input()) for _ in range(M)]
 
-dp = [[float('inf')]*(M+1) for _ in range(N+1)]
+dp = [[float("inf")] * (M + 1) for _ in range(N + 1)]
 
-for i in range(M+1): dp[0][i] = 0
+#init
+for i in range(M + 1):
+    dp[0][i] = 0
 
+#更新
 for i in range(N):
-    for j in range(i+1, M+1):
-        for k in range(0, j):
-            dp[i+1][j] = min(dp[i+1][j], dp[i][k] + C[k]*D[i])
+    for j in range(M):
+        dp[i + 1][j + 1] = min(dp[i + 1][j], dp[i][j] + C[j] * D[i])
+
 
 ans = float('inf')
-for i in range(M+1):
+for i in range(M + 1):
     ans = min(ans, dp[N][i])
 print(ans)
 
-# for row in dp:
-#     print(*row)
+
