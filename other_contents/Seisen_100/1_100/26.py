ac370ad6cbbda3a2a4236cc47e21c92b87680323
hiremasa <syorikunn0901@gmail.com>
2021-05-08 21:35:51 +0900

    26

--- other_contents/Seisen_100/1_100/26.py
+++ other_contents/Seisen_100/1_100/26.py
@@ -6,26 +6,28 @@ dd = [[] for _ in range(N)]
 for _ in range(N - 1):
     a, b = map(int, input().split())
     dd[a - 1].append(b - 1)
+    dd[b - 1].append(a - 1)
+
 
 count = [0] * N
 for _ in range(Q):
     p, x = map(int, input().split())
-    p -= 1
+    count[p - 1] += x
 
-    # init
-    visited = [0] * N
-    stacked_v = deque([p])
-    visited[p] = 1
-    count[p] += x
 
-    #dfs
-    while stacked_v:
-        now_v = stacked_v.popleft()
 
-        for next_v in dd[now_v]:
-            if visited[next_v] == 0:
-                count[next_v] += x
-                visited[next_v] = 1
-                stacked_v.append(next_v)
+# init
+visited = [0] * N
+stacked_v = deque([0])
+
+#dfs
+while stacked_v:
+    now_v = stacked_v.pop()
+    visited[now_v] = 1
+
+    for next_v in dd[now_v]:
+        if visited[next_v] == 0:
+            count[next_v] += count[now_v]
+            stacked_v.append(next_v)
 
 print(*count)
