2fc596a0b72d2126b3e2466ad8db1134a7879485
hiremasa <syorikunn0901@gmail.com>
2021-05-10 00:45:41 +0900

    24

--- other_contents/Seisen_100/1_100/24.py
+++ other_contents/Seisen_100/1_100/24.py
@@ -1,28 +1,34 @@
 N = int(input())
 G = [[] for _ in range(N + 1)]
 
+go_time = [0] * (N + 1)
+back_time = [0] * (N + 1)
+
 for _ in range(N):
     i, num, *nodes = map(int, input().split())
     G[i] = nodes
 
-go_time = [0] * (N + 1)
-back_time = [0] * (N + 1)
 
-def DFS(node, t):
-    #行きの記録
+def dfs(node, t):
+    #行きの処理
     t += 1
     go_time[node] = t
+
+    #再帰処理
     for next_node in G[node]:
         if go_time[next_node] == 0:
-            t = DFS(next_node, t)
+            t = dfs(next_node, t)
 
-    #帰りの記録
+    #帰りの処理
     t += 1
     back_time[node] = t
+
     return t
 
 t = 0
-for node in range(1, N + 1):
-    if go_time[node] == 0:
-        t = DFS(node, t)
-    print(node, go_time[node], back_time[node])
+for i in range(1, N + 1):
+    if go_time[i] == 0:
+        t = dfs(i, t)
+
+for i in range(1, N + 1):
+    print(i, go_time[i], back_time[i])
