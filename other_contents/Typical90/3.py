244286fe43de4f9b5e01cb42e3b888c4551096d8
hiremasa <syorikunn0901@gmail.com>
2021-06-21 23:30:31 +0900

    3.py

--- other_contents/Typical90/3.py
+++ other_contents/Typical90/3.py
@@ -1,28 +1,45 @@
 import sys
 sys.setrecursionlimit(10**6)
+
 N = int(input())
 G = [[] for _ in range(N)]
+
 for _ in range(N - 1):
     a, b = map(int, input().split())
-    a, b = a - 1, b - 1
-    G[a].append(b)
-    G[b].append(a)
+    G[a - 1].append(b - 1)
+    G[b - 1].append(a - 1)
 
 def dfs(s):
+    stack = [s]
     D = [-1] * N
     D[s] = 0
-    stk = [s]
-    push, pop = stk.append, stk.pop
-    while stk:
-        v = pop()
-        nd = D[v] + 1
-        for nv in G[v]:
-            if D[nv] >= 0: continue
-            D[nv] = nd
-            push(nv)
+
+    while stack:
+        node = stack.pop()
+        for next_node in G[node]:
+            if D[next_node] == -1:
+                D[next_node] = D[node] + 1
+                stack.append(next_node)
     return D
 
-D0 = dfs(0)
-mxi = max(enumerate(D0), key=lambda x: x[1])[0]
-Dmx = dfs(mxi)
-print(max(Dmx) + 1)
+# def dfs(s):
+
+#     for next_node in G[s]:
+#         if D[next_node] == -1:
+#             dfs(next_node)
+
+u = max(enumerate(dfs(0)), key=lambda x: x[1])[0]
+ans = max(dfs(u)) + 1
+print(ans)
+
+
+#木かどうか判定
+def dfs(now,prev):
+  global flag
+  for next in g[now]:
+    if next!=prev:
+      if memo[next]:
+        flag=False
+      else:
+        memo[next]=True
+        dfs(next,now)
