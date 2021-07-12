c333b183119ab5520f46110c3ca5bc7ae4b2ba65
hiremasa <syorikunn0901@gmail.com>
2021-07-12 22:54:42 +0900

    d

--- ABC/ABC178/c.py
+++ ABC/ABC178/c.py
@@ -9,7 +9,9 @@ else:
     #     ans *= 10
     #     ans % mod
     # print(ans%mod)
+    ans = (10**(N-2)) % mod
+    print(2 * ans % mod)
 
-    del1 = (9 ** N * 2) % mod - (8 ** N) % mod
-    ans = (10 ** N) % mod - del1
-    print(ans)
+    # del1 = (9 ** N * 2) % mod - (8 ** N) % mod
+    # ans = (10 ** N) % mod - del1
+    # print(ans)
